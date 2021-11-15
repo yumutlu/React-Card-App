f8d8423a48f6299dc98503136a46f57f806782f7
yumutlu <yumutlu@yandex.com>
2021-11-15 15:49:29 +0300

    ShowContentAdd

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,26 +1,40 @@
 import React from "react";
 
-function Collapse(props) {
+class Collapse extends React.Component {
 
-  return (
-    <div>
-    
-        <a
-          className="btn btn-primary w-100"
-          data-bs-toggle="collapse"
-          href={"#".concat(props.href)}
-          role="button"
-          aria-expanded="false"
-          aria-controls="collapseExample"
-        >
-          Link with href
-        </a>
-    
-     
-      <div className="collapse show" id={props.href}>
-      {props.children}
-      </div>
-    </div>
-  );
+    constructor() {
+        super();
+
+this.state={
+    showContent: false
+}
+        
+    }
+  showMore = ()=>{
+      this.setState({showContent:!this.state.showContent})
+  }
+    render(){
+        return (
+            <div>
+            
+                <button className="btn btn-primary w-100" onClick={this.showMore}>
+           
+                
+                  Link with href
+                </button>
+            
+             {
+                 this.state.showContent ? (
+                       <div className="collapse show">
+            {this.props.children}
+
+              </div>  
+                 ):
+                 null
+             }
+          
+            </div>
+          );
+    }
 }
 export default Collapse;
