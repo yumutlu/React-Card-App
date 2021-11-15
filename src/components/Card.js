b56c011f70ddfa8fa4289effebe3452f175f4adf
yumutlu <yumutlu@yandex.com>
2021-11-15 16:47:41 +0300

    End

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,25 +2,20 @@ import React from "react";
 
 const Card = (props) => {
   return (
-  
-      <div className="card w-100">
-        <img src={props.image} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{props.cardTitle}</h5>
-          <p className="card-text">
-           {props.cardText}
-          </p> 
-          <p className="card-text">
-            <small className="text-muted">{props.updateTime}</small>
-          </p>
-        </div>
+    <div className="card w-100">
+      <img src={props.image} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{props.cardTitle}</h5>
+        <p className="card-text">{props.cardText}</p>
+        <p className="card-text">
+          <small className="text-muted">{props.updateTime}</small>
+        </p>
       </div>
-
+    </div>
   );
-}
+};
 
-Card.defaultProps={
-  cardTitle:"Default Props"
-
-}
+Card.defaultProps = {
+  cardTitle: "Default Props  ",
+};
 export default Card;
--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,40 +1,27 @@
 import React from "react";
 
 class Collapse extends React.Component {
+  state = { showContent: false };
 
-    constructor() {
-        super();
-
-this.state={
-    showContent: false
-}
-        
-    }
-  showMore = ()=>{
-      this.setState({showContent:!this.state.showContent})
+  showMore = () => {
+    this.setState({ showContent: !this.state.showContent });
+  };
+  render() {
+    return (
+      <div>
+        <button className="btn btn-primary w-100" onClick={this.showMore}>
+          {React.Children.map(
+            this.props.children,
+            (children) => children.props.cardTitle
+          )}
+        </button>
+        {this.state.showContent ? (
+          <div className="collapse show">
+            {React.Children.map(this.props.children, (children) => children)}
+          </div>
+        ) : null}
+      </div>
+    );
   }
-    render(){
-        return (
-            <div>
-            
-                <button className="btn btn-primary w-100" onClick={this.showMore}>
-           
-                
-                  Link with href
-                </button>
-            
-             {
-                 this.state.showContent ? (
-                       <div className="collapse show">
-            {this.props.children}
-
-              </div>  
-                 ):
-                 null
-             }
-          
-            </div>
-          );
-    }
 }
 export default Collapse;
